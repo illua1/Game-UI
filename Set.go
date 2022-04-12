76d32cc72a0c37ab3a8bc692399275639186bf61
illua1 <fubdmgF@github.com>
2022-04-12 19:38:14 +0300

    fixe Set

--- Set.go
+++ Set.go
@@ -2,14 +2,16 @@ package UI
 
 import (
   "image"
+  slise "github.com/illua1/go-helpful/Slise"
 )
 type RenderingSet struct{
   InSet []Rendering
   Direction_flag bool
+  Offsets []int
 }
 
-func NewSet(flag bool, in ...Rendering)RenderingSet{
-  return RenderingSet{in, flag}
+func NewSet(flag bool, offset []int, in ...Rendering)RenderingSet{
+  return RenderingSet{in, flag, offset}
 }
 
 func(rs RenderingSet)Render(context ScreenContext)error{
@@ -20,16 +22,11 @@ func(rs RenderingSet)Render(context ScreenContext)error{
       }else{
         return nil
       }
+      v, _ := slise.GetLast[int](rs.Offsets, i)
       if rs.Direction_flag {
-        context.SelfRectangle = context.SelfRectangle.Add(image.Point{
-          context.SelfRectangle.Dx(),
-          0,
-        })
-      }else{
-        context.SelfRectangle = context.SelfRectangle.Add(image.Point{
-          0,
-          context.SelfRectangle.Dy(),
-        })
+        context.SelfRectangle = context.SelfRectangle.Add(image.Point{v, 0})
+      } else {
+        context.SelfRectangle = context.SelfRectangle.Add(image.Point{0, v})
       }
     }else{
       return NewRenderError("Nil InSet: ", i)
--- go.mod
+++ go.mod
@@ -3,9 +3,12 @@ module github.com/illua1/Game-UI
 go 1.18
 
 require (
+	github.com/hajimehoshi/ebiten/v2 v2.2.6
+	github.com/illua1/go-helpful v0.0.0-20220412163136-8a13147e7abf
+)
+
+require (
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20210727001814-0db043d8d5be // indirect
-	github.com/hajimehoshi/ebiten/v2 v2.2.6 // indirect
-	github.com/illua1/go-helpful v0.0.0-20220410110929-5355b5f520d7 // indirect
 	github.com/jezek/xgb v1.0.0 // indirect
 	golang.org/x/exp v0.0.0-20220407100705-7b9b53b0aca4 // indirect
 	golang.org/x/exp/shiny v0.0.0-20220407100705-7b9b53b0aca4 // indirect
